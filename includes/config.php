762c8d0e68c6a8f1b064d75e8bd5178c23f299af
EmmaOtoriDesk <chiav0s75xd78@gmail.com>
2026-04-16 02:40:51 +0000

    Config cambiado

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASSWORD') ?: '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
