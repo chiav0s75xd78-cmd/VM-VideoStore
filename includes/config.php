71e56db5cbf68faf8cda7bc9c68fdaa49a12ece2
EmmaOtoriDesk <chiav0s75xd78@gmail.com>
2026-04-16 03:45:54 +0000

    ya arreglate mendiga cochinada

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 
-$host = getenv('DB_HOST') ?: 'localhost';
-$dbname = getenv('DB_NAME') ?: 'videostore';
-$username = getenv('DB_USER') ?: 'root';
-$password = getenv('DB_PASSWORD') ?: '';
+$host = getenv('DB_HOST') ?: 'dpg-d7g4pbtckfvc73b9nif0-a';
+$dbname = getenv('DB_NAME') ?: 'videostore_u48q';
+$username = getenv('DB_USER') ?: 'videouser';
+$password = getenv('DB_PASSWORD') ?: 'rSv9HlVzRaKWvWypOQw6NilPsSTJ8M08';
 
 try {
-    // Forzar SSL para conexión a PostgreSQL en Render
     $dsn = "pgsql:host=$host;dbname=$dbname;sslmode=require";
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
